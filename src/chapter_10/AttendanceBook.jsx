96fa7caa2149518575d6f2ee5e3961891425b81d
changwoon2 <changwoon4@naver.com>
2023-01-18 23:34:47 +0900

    List and Keys

---
+++ src/chapter_10/AttendanceBook.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const students = [
+  {
+    id: 1,
+    name: "민지",
+  },
+  {
+    id: 2,
+    name: "하니",
+  },
+  {
+    id: 3,
+    name: "다니엘",
+  },
+  {
+    id: 4,
+    name: "혜인",
+  },
+];
+
+function AttendanceBook(props) {
+  return (
+    <ul>
+      {students.map((student) => {
+        return <li key={student.id}>{student.name}</li>;
+      })}
+    </ul>
+  );
+}
+
+export default AttendanceBook;
